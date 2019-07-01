ac6a4dae33023c6c3dc16edef3d74d29e65e99c3
Kevin Suen <kevinsuen11@gmail.com>
2019-07-01 20:58:50 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -13,7 +13,7 @@ if (cold) {
 
 console.log("Now you're ready to go outside!\n");
 
-const temperature = -10;
+const temperature = -50;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -24,3 +24,18 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("\nYou are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("\nMaybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("\nLeave your umbrella at home!");
+}
